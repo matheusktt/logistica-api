43c2f19772c8081640db25fcd055d2a0f94b73bb
matheusktt <matheus_0119@outlook.com>
2022-11-02 19:36:12 -0300

    first commit

---
+++ src/main/java/com/logisticaapi/LogisticaApiApplication.java
@@ -0,0 +1,13 @@
+package com.logisticaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logisticaapi/LogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logisticaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
