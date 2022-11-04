57b8e7edb4ce910098750c2427daf3ea5fb0a0a3
matheusktt <matheus_0119@outlook.com>
2022-11-04 19:55:20 -0300

    lombok and crud request

--- src/main/java/com/logisticaapi/controller/ClientController.java
+++ src/main/java/com/logisticaapi/controller/ClientController.java
@@ -2,28 +2,67 @@ package com.logisticaapi.controller;
 
 import com.logisticaapi.domain.model.Client;
 import com.logisticaapi.domain.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
-    @Autowired
     private ClientRepository clientRepository;
 
-    public ClientController(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
-
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> list() {
 //        return clientRepository.findByName("Matheus");
         return clientRepository.findAll();
+    }
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> find(@PathVariable Long clientId) {
+        return clientRepository.findById(clientId)
+                .map(client -> ResponseEntity.ok(client)).orElse(ResponseEntity.notFound().build());
+//              .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+
+//        Optional<Client> client = clientRepository.findById(clientId);
+//        if (client.isPresent()) {
+//            return ResponseEntity.ok(client.get());
+//        }
+//        return ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client addClient(@RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/{clientId}")
+    public ResponseEntity<Client> updateClient(@PathVariable Long clientId, @RequestBody Client client) {
+        if (!clientRepository.existsById(clientId)){
+            return ResponseEntity.notFound().build();
+        }
+        client.setId(clientId);
+        client = clientRepository.save(client);
+
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{clientId}")
+    public ResponseEntity<Void> removeClient(@PathVariable Long clientId) {
+        if (!clientRepository.existsById(clientId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        clientRepository.deleteById(clientId);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/logisticaapi/domain/model/Client.java
+++ src/main/java/com/logisticaapi/domain/model/Client.java
@@ -1,60 +1,23 @@
 package com.logisticaapi.domain.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Objects;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
 @Entity
 public class Client {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String nome) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return id.equals(client.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
